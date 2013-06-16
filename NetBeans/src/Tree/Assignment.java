0bb655eaef666e6602207d188a925397657e5b22
Alina <alina@alina-VPCCA2Z0E.(none)>
2013-06-16 15:57:58 +0100

    assignment constructor

--- NetBeans/src/Tree/Assignment.java
+++ NetBeans/src/Tree/Assignment.java
@@ -1,11 +1,22 @@
 package Tree;
 
+import java.util.ArrayList;
+
 /*
  * @author alina
  * 
  */
 class Assignment {
 
-    Term assignedTerm;
+    ArrayList<Term> assignedTerms = new ArrayList<>();
     Variable boundVar;
+
+    public Assignment(Structure s, Variable var) {
+        this.boundVar = var;
+        for (Term t : s.terms){
+            assignedTerms.add(t);
+        }
+    }
+    
+    
 }
